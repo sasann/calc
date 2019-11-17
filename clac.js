89adb0a70dddd5992be914ae48ff158a3a95eeab
佐々奈津美 <sasanatsumi@sasanatsuminoMacBook-Pro.local>
2019-11-17 17:16:33 +0900

    訂正２

--- clac.js
+++ clac.js
@@ -1,62 +1,50 @@
-<Script language="javascript">
-  ans=0;
-  num="0";
-  key="";
-  kigou="";
 
-    function calc_run(btn) {
-      if(!isNaN(btn)){
-        if(!isNaN(key)){
-            if(num == "0"){
-                num =""+btn;
-            }else{
-                num+=""+btn;
-            }
-        }else{
-              num =""+btn;
-        }
-        document.getElementById("output").innerHTML = num;
-         }
-    }else{
-      if(!isNaN(key)){
-            ans = eval(ans+kigou+num);
-            num ="0";
-            document.getElementById("output").innerHTML =ans;
-          }
-          kigou = btn;
-          document.getElementById("type").innerHTML = kigou;
-
+ans=0;
+num="0";
+key="";
+kigou="";
 
 function calc_run(btn) {
-  if (!isNaN(btn)) {
-    if (!isNaN(key)) {
-      if (num == "0") {
-        num = "" + btn;
-      } else {
-        num += "" + btn;
+  if(!isNaN(btn)){
+    if(!isNaN(key)){
+      if(num == "0"){
+        num =""+btn;
+      }else{
+        num+=""+btn;
       }
-    } else {
-      num = "" + btn;
+    }else{
+      num =""+btn;
     }
-
-
-  function calc_period(){
-        if(num.indexOf(".")<0) num+=".";
-        document.getElementById("output").innerHTML=num;
+    document.getElementById("output").innerHTML = num;
+  }else{
+    if(!isNaN(key)){
+      ans = eval(ans+kigou+num);
+      num ="0";
+      document.getElementById("output").innerHTML =ans;
     }
-   function calc_equal(){
-       if(key == "="){
-           ans =0;
-           key ="";
-       }else{
-           ans = eval(ans + kigou + num);
-           key ="=";
-       }
-       num ="0";
-       kigou = key;
-       document.getElementById("output").innerHTML =ans;
-       document.getElementById("type").innerHTML =key;
-   }
-      
-</Script>
+    kigou = btn;
+    document.getElementById("type").innerHTML = kigou;
+  }
+}
 
+
+function calc_period(){
+  if(num.indexOf(".")<0){
+    num+=".";
+    document.getElementById("output").innerHTML=num;
+  }
+}
+
+function calc_equal(){
+  if(key == "="){
+    ans =0;
+    key ="";
+  }else{
+    ans = eval(ans + kigou + num);
+    key ="=";
+  }
+  num ="0";
+  kigou = key;
+  document.getElementById("output").innerHTML =ans;
+  document.getElementById("type").innerHTML =key;
+}
